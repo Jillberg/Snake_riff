72fbbd7e5ecc8d6165935be3df7e73d226d86836
Hund <105028461+Jillberg@users.noreply.github.com>
2024-09-12 14:52:01 +0200

    project initalized

---
+++ SnakeGame/Assets/Scripts/MapTransition.cs
@@ -0,0 +1,53 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundry;
+    CinemachineConfiner confiner;
+    [SerializeField] float DistanceToTeleport=2f;
+    [SerializeField] Direction direction;
+
+    enum Direction { Down,Left,Right,Up};
+
+    private void Awake()
+    {
+        confiner=FindObjectOfType<CinemachineConfiner>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            Debug.Log("hej");
+            confiner.m_BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += DistanceToTeleport;
+                break;
+            case Direction.Down:
+                newPos.y -= DistanceToTeleport;
+                break;
+            case Direction.Left:
+                newPos.x -= DistanceToTeleport;
+                break;
+            case Direction.Right:
+                newPos.x += DistanceToTeleport;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+}
